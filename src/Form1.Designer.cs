58c84e6b5d7c83048ddc9f4a3a1dba33cb83f53f
Chris Wood <chris.wood@notablygood.com>
2016-07-25 14:21:38 +0100

    Initial Commit

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RedBear.MpanGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMpan = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting MPAN:";
+            // 
+            // txtMpan
+            // 
+            this.txtMpan.Location = new System.Drawing.Point(98, 6);
+            this.txtMpan.Name = "txtMpan";
+            this.txtMpan.Size = new System.Drawing.Size(96, 20);
+            this.txtMpan.TabIndex = 1;
+            this.txtMpan.Text = "2300000000000";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(50, 32);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(144, 26);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate and Copy";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 69);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtMpan);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MPAN Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMpan;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RedBear.MpanGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var mpan = long.Parse(txtMpan.Text);
+            var isValid = false;
+
+            while (!isValid)
+            {
+                mpan += 1;
+                isValid = MpanIsValid(mpan.ToString());
+            }
+
+            txtMpan.Text = mpan.ToString();
+            Clipboard.SetText(txtMpan.Text);
+        }
+
+        private bool MpanIsValid(string mpan)
+        {
+            // Set initial conditions.
+            var validationResult = false;
+
+            if (mpan.Length > 12)
+            {
+
+                //Read the check digit into an Integer variable.
+                int intCheckDigit;
+                if (int.TryParse(mpan.Substring(mpan.Length - 1), out intCheckDigit))
+                {
+                    int[] intPrimes = { 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43 };
+                    var productTotal = 0;
+                    var blnError = false;
+
+                    for (var i = 0; i <= 11; i++)
+                    {
+                        int intTestDigit;
+                        if (int.TryParse(mpan.Substring(i, 1), out intTestDigit))
+                        {
+                            productTotal += (intTestDigit * intPrimes[i]);
+                        }
+                        else
+                        {
+                            blnError = true;
+                            break;
+                        }
+                    }
+
+                    if (!blnError)
+                    {
+                        validationResult = ((productTotal % 11 % 10) == intCheckDigit);
+                    }
+                }
+
+            }
+
+            return validationResult;
+
+        }
+    }
+}
